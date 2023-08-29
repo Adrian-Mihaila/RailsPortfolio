a27f0e78c264587a69d4da0731710af7009816f6
adrianmihailarug <adrianmihaila.imeea@yahoo.com>
2023-08-29 21:17:11 +0300

    Implemented custom scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
+
+    # 2 ways to show only the items with 'Angular' or 'Ruby on Rails' subtitle and add in index method in controller @portfolio_items = Portfolio.angular
+    def self.angular
+        where(subtitle: 'Angular')
+    end
+    
+    # scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 end
