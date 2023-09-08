53ceb9c6335afaf77512f5d464a4ed2136f2869e
adrianmihailarug <adrianmihaila.imeea@yahoo.com>
2023-09-08 16:23:09 +0300

    Implemented logout funtionality and dynamic view rendering for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, path: "", path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
-  get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get "about-me", to: "pages#about"
-  get "contact", to: "pages#contact"
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs do
     member do
@@ -13,6 +13,5 @@ Rails.application.routes.draw do
     end
   end
   
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
