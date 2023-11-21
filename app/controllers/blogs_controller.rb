6626fd2a725b92ade1f8f38ff73c61dde4f052c9
adrianmihailarug <adrianmihaila.imeea@yahoo.com>
2023-11-21 19:46:38 +0200

    Updated layouts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -33,9 +33,11 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Your post is now live.' }
+        format.html { redirect_to blog_url(@blog), notice: "Blog was successfully created." }
+        format.json { render :show, status: :created, location: @blog }
       else
-        format.html { render :new }
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -45,9 +47,11 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.html { redirect_to blog_url(@blog), notice: "Blog was successfully updated." }
+        format.json { render :show, status: :ok, location: @blog }
       else
-        format.html { render :edit }
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,7 +61,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully removed.' }
       format.json { head :no_content }
     end
   end
