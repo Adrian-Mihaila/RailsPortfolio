72f084894a88de6515874e9b6a0a2317586da12f
adrianmihailarug <adrianmihaila.imeea@yahoo.com>
2023-11-22 20:11:03 +0200

    Updated migrations

--- db/migrate/20161230234042_remove_test_items.rb
+++ db/migrate/20161230234042_remove_test_items.rb
@@ -1,4 +1,4 @@
-class RemoveTestItems < ActiveRecord::Migration[5.0]
+class RemoveTestItems < ActiveRecord::Migration[7.0]
   def change
     remove_column :blogs, :status, :integer
     remove_column :blogs, :test_status, :integer
