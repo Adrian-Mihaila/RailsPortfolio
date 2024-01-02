53de8affcb598d19be2115d602bb1af978fba9bc
adrianmihailarug <adrianmihaila.imeea@yahoo.com>
2024-01-02 13:32:06 +0200

    Deleted unnecesary comments from social tool

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -1,30 +1,4 @@
 require "x"
-
-# module SocialTool
-#   def self.twitter_search
-#     x_credentials = {
-#     api_key:             ENV.fetch("TWITTER_CONSUMER_KEY"),
-#     api_key_secret:      ENV.fetch("TWITTER_CONSUMER_SECRET"),
-#     access_token:        ENV.fetch("TWITTER_ACCESS_TOKEN"),
-#     access_token_secret: ENV.fetch("TWITTER_ACCESS_SECRET"),
-#     }
-
-#     x_client = X::Client.new(**x_credentials)
-
-#     search_endpoint = "search/tweets.json?q=%23rubyonrails&result_type=recent&count=6"
-
-#     # Make the request to search for tweets
-#     search_result = x_client.get(search_endpoint)
-
-#     # Extract relevant information from the search result
-#     tweets = search_result["data"]["statuses"]
-
-#     tweets.map do |tweet|
-#       "#{tweet["user"]["screen_name"]}: #{tweet["text"]}"
-#     end
-#   end
-# end
-
 require 'net/http'
 require 'uri'
 require 'json'
@@ -42,7 +16,6 @@ module SocialTool
     begin
       x_client = X::Client.new(**x_credentials)
 
-      # Example: Change this to your actual search query
       search_query = "#coding"
 
       uri = URI.parse("https://api.twitter.com/2/tweets/search/recent?query=#{URI.encode_www_form_component(search_query)}&max_results=6")
@@ -54,9 +27,6 @@ module SocialTool
 
       response = http.request(request)
       parsed_response = JSON.parse(response.body)
-
-      # Process the results...
-      # (Your logic to handle the response)
 
     rescue X::Forbidden => e
       puts "Forbidden Error: #{e.message}"
